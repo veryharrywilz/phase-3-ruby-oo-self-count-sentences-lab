0b44bd59a47a3e278f4f49f602ca65b6dae21880
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-01 21:35:40 -0400

    commit tree c85e5de6568bd039b42ae7c088da03e806b64b67

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
+  def initialize(inputString)
+    self.string=inputString
+  end
+
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentenceArray = self.delete(",").split(/\.|\?|!/).filter{ |sentence| !sentence.empty? }
+    sentenceArray.count()
   end
 end
+
+# binding.pry
